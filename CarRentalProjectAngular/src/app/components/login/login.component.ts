a0f0b40b159996f1ab4b5a316724ec8a00b00a6c
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-04-26 15:05:52 +0300

    Login

--- CarRentalProjectAngular/src/app/components/login/login.component.ts
+++ CarRentalProjectAngular/src/app/components/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  auth$!: Observable<boolean>;
 
   ngOnInit(): void {
+    this.auth$ = this.authService.authAsObservable();
   }
 
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
