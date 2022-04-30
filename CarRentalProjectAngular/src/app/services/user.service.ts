7c24de2e943c34e2a89783ad8011ad69a4d0684b
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-04-30 22:11:35 +0300

    Seperating validation form retrieval

---
+++ CarRentalProjectAngular/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { LoginCredentials } from '../models/login-credentials';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) {
+    this.serverURL = environment.serverURL;
+  }
+  private serverURL: string;
+  public userObservable$!: Observable<User>
+  
+  getUserObservable(loginCredentials: LoginCredentials){
+    this.userObservable$ = this.httpClient.get<User>(
+      `${this.serverURL}Login/${loginCredentials.loginId},${loginCredentials.password}`
+    );
+  }
+}
