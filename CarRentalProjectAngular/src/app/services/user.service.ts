4be5967d94516c535d45c2c61720c53008fdecc1
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-05 20:49:18 +0300

    User is now empty

--- CarRentalProjectAngular/src/app/services/user.service.ts
+++ CarRentalProjectAngular/src/app/services/user.service.ts
@@ -1,25 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { LoginCredentials } from '../models/login-credentials';
-import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClient: HttpClient) {
-    this.serverURL = environment.serverURL;
-  }
-  //Should be public
-  public user: User | null = null;
-  private serverURL: string;
 
-  getUser(loginCredentials: LoginCredentials) {
-    this.httpClient
-      .get<User>(
-        `${this.serverURL}Login/${loginCredentials.loginId},${loginCredentials.password}`
-      )
-      .subscribe((user) => this.user = user);
-  }
 }
