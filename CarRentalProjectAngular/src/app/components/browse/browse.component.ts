b03690b93389af08b3fc6a2d6055125b23141e15
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-08 17:59:37 +0300

    Implemented OnDestroy

--- CarRentalProjectAngular/src/app/components/browse/browse.component.ts
+++ CarRentalProjectAngular/src/app/components/browse/browse.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Vehicle } from 'src/app/models/vehicle';
+import { VehicleType } from 'src/app/models/vehicle-type';
+import { BrowseService } from 'src/app/services/browse.service';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.css']
 })
-export class BrowseComponent implements OnInit {
+export class BrowseComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  constructor(private browseService: BrowseService) { }
 
   ngOnInit(): void {
+    this.browseService.loadAllAvailableVehicles();
+    this.browseService.loadAllAvailableVehicleTypes();
+    this.availableVehicles$ = this.browseService.getAvailableVehiclesAsObservable();
+    this.availableVehiclesTypes$ = this.browseService.getAvailableVehicleTypesAsObservable();
+  }
+  availableVehicles$!: Observable<Vehicle[]>;
+  availableVehiclesTypes$!: Observable<VehicleType[]>;
+
+  //temp
+  reload(){
+    this.browseService.loadAllAvailableVehicles()
   }
 
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 }
