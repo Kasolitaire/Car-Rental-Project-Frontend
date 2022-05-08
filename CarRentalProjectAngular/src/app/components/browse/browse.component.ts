73cd985eb53d68fabc6d9749d936d6fbd5d85ff0
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-08 18:01:48 +0300

    Array that contains all available vehicles
    Array that contains types of all available vehicles'

--- CarRentalProjectAngular/src/app/components/browse/browse.component.ts
+++ CarRentalProjectAngular/src/app/components/browse/browse.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Vehicle } from 'src/app/models/vehicle';
 import { VehicleType } from 'src/app/models/vehicle-type';
 import { BrowseService } from 'src/app/services/browse.service';
@@ -7,23 +7,25 @@ import { BrowseService } from 'src/app/services/browse.service';
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.css']
+  styleUrls: ['./browse.component.css'],
 })
-export class BrowseComponent implements OnInit , OnDestroy{
-
-  constructor(private browseService: BrowseService) { }
+export class BrowseComponent implements OnInit, OnDestroy {
+  constructor(private browseService: BrowseService) {}
 
   ngOnInit(): void {
+    //HTTP requests
     this.browseService.loadAllAvailableVehicles();
     this.browseService.loadAllAvailableVehicleTypes();
 
+
   }
 
-
-
-
+  
+  //Vehicle Data Properties
+  private availableVehiclesList!: Vehicle[];
+  private availableVehicleTypesList!: VehicleType[];
 
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+
   }
 }
