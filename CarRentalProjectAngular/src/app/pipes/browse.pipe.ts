086be94b205ba84ba11a32741982573c2e59431b
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-10 12:41:27 +0300

    Filter Vehicles Pipe Logic

--- CarRentalProjectAngular/src/app/pipes/browse.pipe.ts
+++ CarRentalProjectAngular/src/app/pipes/browse.pipe.ts
@@ -1,12 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { map, of } from 'rxjs';
+import {
+  VehicleFilterRequirements,
+  VehicleRequirements,
+} from '../models/vehicle-filter-requirements';
+import { VehicleType } from '../models/vehicle-type';
 
 @Pipe({
-  name: 'browse'
+  name: 'browse',
 })
 export class BrowsePipe implements PipeTransform {
+  transform (vehicleType: VehicleType, vehicleFilterRequirements: VehicleFilterRequirements) {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    let vehicleDataList = [];
+    let filterDataList = [];
+
+    for (let key in vehicleFilterRequirements.vehicleRequirements) {
+
+      let filterKey: keyof VehicleRequirements = key as keyof typeof vehicleFilterRequirements.vehicleRequirements;
+      vehicleDataList.push(vehicleType[filterKey].toUpperCase());
+      filterDataList.push(vehicleFilterRequirements.vehicleRequirements[filterKey].toUpperCase())
+    }
+
+    for (let index = 0; index < vehicleDataList.length; index++) {
+      if(vehicleDataList[index] != filterDataList[index] && filterDataList[index] != ''){
+        return false
+      }
+    }
+
+    return vehicleDataList.includes(vehicleFilterRequirements.generalRequirement.toUpperCase()) || vehicleFilterRequirements.generalRequirement == '';
   }
-
 }
