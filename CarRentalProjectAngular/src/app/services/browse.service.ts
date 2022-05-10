b5ec62ea857825cf32531cb47c84d000104cd608
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-10 17:33:42 +0300

    ReplaySubject

--- CarRentalProjectAngular/src/app/services/browse.service.ts
+++ CarRentalProjectAngular/src/app/services/browse.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
+import { BehaviorSubject, firstValueFrom, Observable, ReplaySubject, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Vehicle } from '../models/vehicle';
 import { VehicleType } from '../models/vehicle-type';
@@ -11,39 +11,53 @@ import { VehicleType } from '../models/vehicle-type';
 export class BrowseService {
 
   constructor(private httpClient :HttpClient) { }
-
+  //Server URL from environment
   private serverURL: string = environment.serverURL;
-  private vehicleSubject$: BehaviorSubject<Vehicle[]> = new BehaviorSubject<Vehicle[]>([])
-  private vehicleTypeSubject$: BehaviorSubject<VehicleType[]> = new BehaviorSubject<VehicleType[]>([]);
 
-  async loadAllAvailableVehicles(){ // gets all the available vehicles via api
+  //Emits all available vehicles
+  private availableVehiclesSubject$: BehaviorSubject<Vehicle[]> = new BehaviorSubject<Vehicle[]>([])
+  //Emits all available vehicles types
+  private availableVehicleTypesSubject$: BehaviorSubject<VehicleType[]> = new BehaviorSubject<VehicleType[]>([]);
+  //Emits the selected vehicle
+  private selectedVehicleTypeReplay$: ReplaySubject<VehicleType> = new ReplaySubject<VehicleType>(1);
+
+  //Retrieves all the available vehicles from the server
+  async loadAllAvailableVehicles(){
     try {
       const availableVehicles$: Observable<Vehicle[]> = this.httpClient.get<Vehicle[]>(this.serverURL + 'Vehicle/GetAvailableVehicles');
       const availableVehiclesList: Vehicle[] = await firstValueFrom(availableVehicles$);
-      this.vehicleSubject$.next(availableVehiclesList);
+      this.availableVehiclesSubject$.next(availableVehiclesList);
     }
     catch (error) {
       console.log((error as HttpErrorResponse).status);
     }
   }
 
-  async loadAllAvailableVehicleTypes(){ // gets all the available vehicle types that have available vehicles
+   //Retrieves all the available vehicles types from the server
+  async loadAllAvailableVehicleTypes(){
     try {
       const availableVehicleTypes$: Observable<VehicleType[]> = this.httpClient.get<VehicleType[]>(this.serverURL + 'Vehicle/GetAllAvailableVehicleTypes');
       const availableVehicleTypeList: VehicleType[] = await firstValueFrom(availableVehicleTypes$);
-      this.vehicleTypeSubject$.next(availableVehicleTypeList);
+      this.availableVehicleTypesSubject$.next(availableVehicleTypeList);
     }
     catch (error) {
       console.log((error as HttpErrorResponse).status);
     }
   }
 
+  emitSelectedVehicle(selectedVehicleType: VehicleType){
+    this.selectedVehicleTypeReplay$.next(selectedVehicleType);
+  }
+
+  getSelectedVehicleTypeAsObservable(){
+    return this.selectedVehicleTypeReplay$.asObservable();
+  }
+
   getAvailableVehiclesAsObservable(){
-    return this.vehicleSubject$.asObservable();
+    return this.availableVehiclesSubject$.asObservable();
   }
 
   getAvailableVehicleTypesAsObservable(){
-    return this.vehicleTypeSubject$.asObservable()
+    return this.availableVehicleTypesSubject$.asObservable()
   }
-
 }
