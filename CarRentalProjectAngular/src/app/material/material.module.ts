5fd4650dd9d3b0bdb10d47a634eebaf170265378
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-11 18:18:23 +0300

    Material datepicker

--- CarRentalProjectAngular/src/app/material/material.module.ts
+++ CarRentalProjectAngular/src/app/material/material.module.ts
@@ -3,11 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
 ];
 
 @NgModule({
