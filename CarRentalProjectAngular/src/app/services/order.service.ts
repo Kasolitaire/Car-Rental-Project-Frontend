55c6c7d93698e8b8e07b1a627f29d8e58b2be3f1
Kasolitaire <90893226+Kasolitaire@users.noreply.github.com>
2022-05-11 18:16:35 +0300

    Order service in charge or making oder

---
+++ CarRentalProjectAngular/src/app/services/order.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable, zip } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { OrderDetail } from '../models/order';
+import { User } from '../models/user';
+import { Vehicle } from '../models/vehicle';
+import { VehicleType } from '../models/vehicle-type';
+import { AuthService } from './auth.service';
+import { BrowseService } from './browse.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService{
+
+  constructor(private browseService: BrowseService, private authService: AuthService, private httpClient: HttpClient) { }
+
+  serverURL:String = environment.serverURL;
+
+  async executeOrder(){
+    const availableVehicles$: Observable<Vehicle[]> = this.browseService.getAvailableVehiclesAsObservable()
+    const selectedVehicleType$: Observable<VehicleType> = this.browseService.getSelectedVehicleTypeAsObservable()
+    const currentUser$: Observable<User> = this.authService.userAsObservable()
+    zip(availableVehicles$, selectedVehicleType$, currentUser$).subscribe(([emittedVehicles, emittedType, emittedUser]) =>{
+      const matchingVehicle:Vehicle | undefined = this.findMatchingVehicle(emittedVehicles, emittedType);
+      const order = this.createOrder(matchingVehicle, emittedUser);
+      this.postOrder(order, matchingVehicle)
+    }).unsubscribe();
+  }
+
+  private findMatchingVehicle(availableVehiclesList: Vehicle[], selectedVehicleType: VehicleType): Vehicle | undefined {
+    return availableVehiclesList.find(
+      (vehicle) => vehicle.vehicleTypeId == selectedVehicleType.vehicleTypeId
+    );
+  }
+
+  private createOrder(matchingVehicle:Vehicle | undefined, currentUser: User){
+    return {
+    pickUpDate: "PickUpDate",
+    dropOffDate:"DropOffDate",
+    userId: +currentUser.id,
+    serialNumber: matchingVehicle?.serialNumber
+    } as OrderDetail
+  }
+
+  async postOrder(order: OrderDetail, matchingVehicle:Vehicle | undefined){
+    //post
+    try {
+      debugger
+      const response$ = this.httpClient.post(`${this.serverURL}User/PostNewOrder,${matchingVehicle?.vehicleId}`, order);
+      console.log(await firstValueFrom(response$));
+    }
+    catch (error) {
+      console.log((error as HttpErrorResponse));
+    }
+    finally{
+      this.browseService.loadAllAvailableVehicleTypes();
+      this.browseService.loadAllAvailableVehicles();
+    }
+  }
+}
